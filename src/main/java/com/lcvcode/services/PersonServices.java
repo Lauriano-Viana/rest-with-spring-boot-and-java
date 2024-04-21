b89b291f20f0e48551e93823512420b09bc24b8f
Lariano <laurianoviana12@gmail.com>
2024-04-21 14:41:07 -0300

    metodos findById e findAll

--- src/main/java/com/lcvcode/services/PersonServices.java
+++ src/main/java/com/lcvcode/services/PersonServices.java
@@ -3,6 +3,8 @@ package com.lcvcode.services;
 import com.lcvcode.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -11,6 +13,18 @@ public class PersonServices {
 
     private final AtomicLong counter = new AtomicLong();
     private Logger logger = Logger.getLogger(PersonServices.class.getName());
+
+    public List<Person> findAll() {
+        logger.info("Finding all peoples");
+        List<Person> persons = new ArrayList<>();
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
+
 
     public Person findById(String id) {
         logger.info("Finding one person!");
@@ -22,4 +36,16 @@ public class PersonServices {
         person.setGender("Male");
         return person;
     }
+
+    private Person mockPerson(int i) {
+
+        var person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("person name " + i);
+        person.setLastName("Las name " + i);
+        person.setAdress("Some adress in Brasil" + i);
+        person.setGender("Male");
+        return person;
+    }
+
 }
